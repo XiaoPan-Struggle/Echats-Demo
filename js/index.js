f4459f1da45bd7f5325d9dde8427889bd25d1e4b
XiaoPan <2489964425@qq.com>
2020-11-18 22:23:21 +0800

    地区分部-饼形图 完成

--- js/index.js
+++ js/index.js
@@ -491,4 +491,47 @@
   window.addEventListener('resize', function (){
     myEchart.resize()
   })
-})()
+})();
+
+
+/* 地区分部 */
+(function (){
+  var myEchart = echarts.init(document.querySelector('.pie2 .chart'))
+
+  var option = {
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+        left: 'center',
+        top: 'bottom',
+        data: ['rose1', 'rose2', 'rose3', 'rose4', 'rose5', 'rose6', 'rose7', 'rose8']
+    },
+    series: [
+        {
+            name: '面积模式',
+            type: 'pie',
+            radius: [10, 100],
+            center: ['50%', '50%'],
+            roseType: 'area',
+            color: ['#006cff', '#60cda0', '#ed8884', '#ff9f7f', '#0096ff', '#9fe6b8', '#32c5e9', '#1d9dff'],
+            data: [
+                {value: 50, name: '湖北'},
+                {value: 40, name: '四川'},
+                {value: 15, name: '浙江'},
+                {value: 25, name: '北京'},
+                {value: 20, name: '上海'},
+                {value: 35, name: '云南'},
+                {value: 30, name: '河北'},
+                {value: 35, name: '江苏'}
+            ]
+        }
+    ]
+  };
+  myEchart.setOption(option)
+
+  window.addEventListener('resize',function() {
+    myEchart.resize()
+  })
+})();
